d9fa330e0528808932dc19506bda18e1ad7828c3
roangira99 <angirarobert7@gmail.com>
2023-03-09 08:14:21 +0300

    Added bubble sort, built in sorted function and functionality to allow for multiple runs

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,4 @@
-from demos import quicksort, merge_sort
+from demos import quicksort, merge_sort, bubble_sort
 import random
 import time
 
@@ -8,19 +8,24 @@ def create_random_list(size, max_val):
         ran_list.append(random.randint(1, max_val)) # append each number to the list
     return ran_list 
 
+def analyze_func(func_name, arr):
+    tic = time.time()
+    func_name(arr)
+    toc = time.time()
+    seconds = toc - tic
+    print(f"{func_name.__name__.capitalize()}\t-> Elapsed time: {seconds:.5f}")  # The dunder method gives you just the name of the function instead of the string representation of the object
+    # :.5f takes the value in the seconds variable and limits it to 5 decimal places
+
 size = int(input("What size of list do you want to create? "))
 max = int(input("What maximum value do you want for the range? "))
+run_times = int(input("How many times do you want to run? "))
 
-l = create_random_list(size, max)
-
-tic = time.time()
-quicksort(l)
-toc = time.time()
-print(f"Quick sort Elapsed time -> {toc - tic}")
-
-tic = time.time()
-merge_sort(l)
-toc = time.time()
-print(f"Merge sort Elapsed time -> {toc - tic}")
-
+for num in range(run_times):
+    print(f"Run {num+1}")
+    l = create_random_list(size, max)
+    # analyze_func(bubble_sort, l.copy()) # the l.copy method ensures bubble sort doesn't sort the original list (sorting the list in place)
+    analyze_func(quicksort, l) # the first argument passes in the function as an object to analyze_func
+    # analyze_func(merge_sort, l) 
+    analyze_func(sorted, l)
+    print("-" * 50)
 
--- demos.py
+++ demos.py
@@ -45,3 +45,13 @@ def merge_sort(arr):
         l1 = merge_sort(arr[:middle])
         l2 = merge_sort(arr[middle:])
         return merge_sorted(l1, l2)
+    
+def bubble_sort(arr):
+    swap_happened = True # Flag that checks whether a swap happened
+    while swap_happened:
+        swap_happened = False
+        # the loop should go to the lastElement - 1 because after the lastElement there is nothing to compare with
+        for num in range(len(arr) - 1):
+            if arr[num] > arr[num + 1]:
+                swap_happened = True
+                arr[num], arr[num + 1] = arr[num + 1], arr[num] # Dynamic swapping
